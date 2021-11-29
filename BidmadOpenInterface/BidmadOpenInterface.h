5c804d6f6e3075890c045fdc5d9840f0d7d5b16a
MarkusOhADOP <markus@adop.cc>
2021-11-29 17:41:30 +0900

    Initial Commit

---
+++ BidmadOpenInterface/BidmadOpenInterface.h
@@ -0,0 +1,18 @@
+//
+//  BidmadOpenInterface.h
+//  BidmadOpenInterface
+//
+//  Created by ADOP_Mac on 2021/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BidmadOpenInterface.
+FOUNDATION_EXPORT double BidmadOpenInterfaceVersionNumber;
+
+//! Project version string for BidmadOpenInterface.
+FOUNDATION_EXPORT const unsigned char BidmadOpenInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BidmadOpenInterface/PublicHeader.h>
+
+
---
+++ BidmadOpenInterfaceExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BidmadOpenInterfaceExamples
+//
+//  Created by ADOP_Mac on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
